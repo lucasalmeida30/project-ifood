0ce21e1d9bd18e749e1fb584cf99a1a4658ba32c
lucasalmeida30 <lucas_almeida12@icloud.com>
2024-04-30 14:28:15 -0300

    feat: add category item and list components

---
+++ app/_components/categoryList.tsx
@@ -0,0 +1,16 @@
+import { db } from "../_lib/prisma";
+import CategoryItem from "./categoryItem";
+
+const CategoryList = async () => {
+  const categories = await db.category.findMany({});
+
+  return (
+    <div className="my-2 flex flex-row gap-3 overflow-scroll px-5 py-2 [&::-webkit-scrollbar]:hidden">
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
+};
+
+export default CategoryList;
