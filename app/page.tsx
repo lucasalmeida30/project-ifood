641170084c3f7b09f12366bd9639917faf3d3af4
lucasalmeida30 <lucas_almeida12@icloud.com>
2024-04-30 14:55:29 -0300

    feat: add promo banner to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import CategoryList from "./_components/categoryList";
 import Header from "./_components/header";
 import Search from "./_components/search";
@@ -12,6 +13,18 @@ export default function Home() {
 
       <div className="pt-6">
         <CategoryList />
+      </div>
+
+      <div className="pt-2">
+        <Image
+          src="/Banner-promo01.png"
+          alt="Até 30% de desconto em pizzas"
+          height={0}
+          width={0}
+          sizes="100vw"
+          className="h-auto w-full object-contain"
+          quality={100}
+        />
       </div>
     </div>
   );
